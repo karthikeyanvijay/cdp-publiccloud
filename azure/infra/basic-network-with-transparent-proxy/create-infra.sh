ecc2f8b6c347897f10373b3729ef26ae5b41bae6
Vijay Anand Karthikeyan <vijayanand.karthikeyan@gmail.com>
2021-04-19 20:43:50 -0500

    Initial commit for Azure

---
+++ azure/infra/basic-network-with-transparent-proxy/create-infra.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Wrapper script to create Infrastructure
+# Author: Vijay Anand Karthikeyan
+
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <resource-group-name>"
+    exit 1
+fi
+
+az deployment group create --resource-group ${1} --template-file azuredeploy.json --parameters azuredeploy.parameters.json
