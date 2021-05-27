745049d07e7ba64386acafdd55ec9c7ea4002166
karthikeyanvijay <vijayanand.karthikeyan@gmail.com>
2021-05-27 22:47:29 +0000

    Added Azure ARM network template with firewall draft

---
+++ azure/ARM-network-firewall/create-infra.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Wrapper script to create Infrastructure
+# Author: Vijay Anand Karthikeyan
+
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <resource-group-name>"
+    exit 1
+fi
+
+echo "Work in progress..exiting"
+exit 1
+
+az deployment group create  \
+        --resource-group ${1} \
+        --template-file azuredeploy.json \
+        --parameters azuredeploy.parameters.json \
+        --mode Complete \
+        --parameters vmAdminPassword='BadPass#1' AllowIPAddress='71.135.69.90/32' deployVMinPrivateSubnet=false
+        
