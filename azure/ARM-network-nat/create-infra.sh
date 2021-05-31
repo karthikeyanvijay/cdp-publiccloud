9cdd892c3a92ec8f911d37bcb5bce6b5acf1f879
karthikeyanvijay <vijayanand.karthikeyan@gmail.com>
2021-05-31 21:45:36 +0000

    Update for Azure

--- azure/ARM-network-nat/create-infra.sh
+++ azure/ARM-network-nat/create-infra.sh
@@ -3,14 +3,14 @@
 # Author: Vijay Anand Karthikeyan
 
 source config_file
-if [ "x" == "x$namePrefix" ] || [ "x" == "x$MyIpAddressCIDR" ] || [ "x" == "x$ResourceGroup" ] || [ "x" == "x$vmAdminPassword" ] || [ "x" == "x$deployVMinPrivateSubnet" ] || [ "x" == "x$deployBastionVM" ] || [ "x" == "x$resourceTags" ]; then
+if [ "x" == "x$namePrefix" ] || [ "x" == "x$MyIpAddressCIDR" ] || [ "x" == "x$ResourceGroup" ] || [ "x" == "x$vmAdminPassword" ] || [ "x" == "x$deployVMinPrivateSubnet" ] || [ "x" == "x$deployBastionVM" ] || [ "x" == "x$resourceTags" ] || [ "x" == "x$setupPeeredNetwork" ] || [ "x" == "x$vmCountinPeeredNetwork" ]; then
   echo "Ensure that the required variables are set in the config_file"
   exit 1
 fi
 
 az deployment group create  \
-        --resource-group ${ResourceGroup} \
-        --template-file azuredeploy.json \
-        --parameters azuredeploy.parameters.json \
-        #--mode Complete \
-        --parameters namePrefix=${namePrefix} vmAdminPassword=${vmAdminPassword} AllowIPAddress=${MyIpAddressCIDR}  deployVMinPrivateSubnet=${deployVMinPrivateSubnet} deployBastionVM=${deployBastionVM} resourceTags="${resourceTags}"
+      --resource-group ${ResourceGroup} \
+      --template-file azuredeploy.json \
+      --parameters azuredeploy.parameters.json \
+      --parameters namePrefix=${namePrefix} vmAdminPassword=${vmAdminPassword} AllowIPAddress=${MyIpAddressCIDR} deployVMinPrivateSubnet=${deployVMinPrivateSubnet} deployBastionVM=${deployBastionVM} resourceTags="${resourceTags}" setupPeeredNetwork=${setupPeeredNetwork}  setupPeeredNetworkVMs=${setupPeeredNetworkVMs} vmCountinPeeredNetwork=${vmCountinPeeredNetwork} \
+      --mode Complete
